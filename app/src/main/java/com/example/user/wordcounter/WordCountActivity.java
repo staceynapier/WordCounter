972c1591ad3a9e05c134d1430936001420ea4409
staceynapier <staceynapier86@gmail.com>
2017-08-08 17:54:22 +0100

    created basic view for home and answer page

--- app/src/main/java/com/example/user/wordcounter/WordCountActivity.java
+++ app/src/main/java/com/example/user/wordcounter/WordCountActivity.java
@@ -2,12 +2,29 @@ package com.example.user.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class WordCountActivity extends AppCompatActivity {
+
+    EditText counterEditText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_count);
+
+        counterEditText = (EditText)findViewById(R.id.counter_text);
+        button = (Button)findViewById(R.id.button);
+    }
+
+    public void onPressButton(View button){
+        Log.d(getClass().toString(), "Button was pressed");
+        String counter = counterEditText.getText().toString();
+        Log.d(getClass().toString(), "The words entered were '" + counter + "'");
     }
 }
